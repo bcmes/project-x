8a91d399fb908e64ec02de1803ca664263c392eb
bcmes <brunomeloesilva@gmail.com>
2024-09-02 21:26:43 -0400

    initial-commit

---
+++ src/test/java/com/github/bcmes/orders/OrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.bcmes.orders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
