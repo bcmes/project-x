6ccef785fc99757c3af85d12aad97895cb3455d1
Bruno Silva <brunomeloesilva@gmail.com>
2024-09-07 22:12:05 -0400

    chore: test

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,6 +2,5 @@
 --    id INT PRIMARY KEY AUTO_INCREMENT,
 --    name VARCHAR(100) NOT NULL
 --);
-
 INSERT INTO client (id, name) VALUES (uuid(), 'Iron Man');
 INSERT INTO client (id, name) VALUES (uuid(), 'Doctor Strange');
